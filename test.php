c7874dae67a69f6102fbf285e5444db6c82402f2
Kaj-Michael Lang <milang@tal.org>
2015-10-15 00:05:13 +0300

    Add simple test

---
+++ test.php
@@ -0,0 +1,21 @@
+#!/usr/bin/php -q
+<?php
+require_once('lib/YleApi.php');
+
+if (file_exists("config.ini"))
+	$config=parse_ini_file("config.ini", true);
+else
+	die('Configuration file config.ini is missing');
+
+$api=$config['Generic'];
+
+$c=new YleApiClient($api['app_id'], $api['app_key'], '');
+$c->set_debug(true);
+
+$a=$c->programs_services();
+file_put_contents('services.txt', json_encode($a, JSON_PRETTY_PRINT));
+
+$a=$c->programs_items();
+file_put_contents('items.txt', json_encode($a, JSON_PRETTY_PRINT));
+
+?>
