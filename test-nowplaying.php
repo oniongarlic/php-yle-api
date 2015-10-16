add605eda30089adbfdb70a129060fe0e1ab4f66
Kaj-Michael Lang <milang@tal.org>
2015-10-16 15:05:30 +0300

    A simple test to dump nowplaying info for given channel id

---
+++ test-nowplaying.php
@@ -0,0 +1,21 @@
+#!/usr/bin/php -q
+<?php
+require_once('lib/YleApi.php');
+
+if (file_exists("config.ini"))
+	$config=parse_ini_file("config.ini", true);
+else
+	die("Configuration file config.ini is missing\n");
+
+if ($argc!==2)
+	die("Channel ID required!\n");
+
+$pid=$argv[1];
+$api=$config['Generic'];
+
+$c=new YleApiClient($api['app_id'], $api['app_key'], $api['decrypt']);
+$c->set_debug(true);
+
+$a=$c->programs_nowplaying($pid);
+print_r($a);
+?>
