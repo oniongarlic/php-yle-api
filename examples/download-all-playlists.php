377a77c7a9af5508bb92a0b6a2b419de1003ac98
Kaj-Michael Lang <milang@tal.org>
2016-11-04 17:13:39 +0200

    Support other channels too

--- examples/download-all-playlists.php
+++ examples/download-all-playlists.php
@@ -6,10 +6,19 @@ require_once('../lib/YleScrobblerParser.php');
 // Base url is https://svenska.yle.fi/spellista/yle-x3m/
 // Source is https://svenska.yle.fi/spellista/yle-x3m/2013-06-07
 
+class PlaylistCSVSaver
+{
+private $cid;
+
+function __construct($cid)
+{
+$this->cid=$cid;
+}
+
 // Save unique artists
 function save_artists(array $artists)
 {
-$fp = fopen('artists.csv', 'w');
+$fp = fopen($this->cid.'-artists.csv', 'w');
 fputcsv($fp, array('a-hash','artist','related-all','r1','r2','r3','r4','r5'));
 foreach ($artists as $id => $a) {
  $t=array_merge(array($id), $a);
@@ -23,7 +32,7 @@ fclose($fp);
 function save_songs(array $songs)
 {
 
-$fp = fopen('songs.csv', 'w');
+$fp = fopen($this->cid.'-songs.csv', 'w');
 fputcsv($fp, array('sa-hash','a-hash','artist','song','count'));
 foreach ($songs as $id=>$s) {
  fputcsv($fp, array($id, $s['ahash'], $s['artist'], $s['song'], $s['count']));
@@ -34,7 +43,7 @@ fclose($fp);
 // Save unique progams that played
 function save_programs(array $programs)
 {
-$fp = fopen('programs.csv', 'w');
+$fp = fopen($this->cid.'-programs.csv', 'w');
 fputcsv($fp, array('program'));
 foreach ($programs as $a) {
  fputcsv($fp, array($a));
@@ -45,7 +54,7 @@ fclose($fp);
 // Save playlists
 function save_playlists(array $playlists)
 {
-$fp = fopen('playlist.csv', 'w');
+$fp = fopen($this->cid.'-playlist.csv', 'w');
 fputcsv($fp, array('date','sa-hash','artist','a-hash','song','program','channel','time','duration'));
 foreach ($playlists as $date=>$songs) {
  foreach ($songs as $song) {
@@ -57,66 +66,83 @@ foreach ($playlists as $date=>$songs) {
 fclose($fp);
 }
 
+} // class
+
+function parse_playlist($base, $cache, &$p, DateTime &$from, DateTime &$to)
+{
+$ymd=$from->format('Y-m-d');
+
+echo "$ymd";
+
+$url=sprintf('%s%s', $base, $ymd);
+
+$cf=$cache.$ymd;
+$cached=false;
+
+if (!file_exists($cf)) {
+  echo "-d";
+  $data=file_get_contents($url);
+} else {
+  echo "-c";
+  $cached=true;
+  $data=file_get_contents($cf);
+}
+
+if (!$p->parseHTML($data)) {
+  echo "?";
+} else if (!$cached && ($from<$to)) {
+  // Cache the downloaded data only if parsing was ok
+  echo "!";
+  file_put_contents($cf, $data);
+}
+$data=null;
+echo "\n";
+}
+
 ///////////////////////////////////////////////////////////////////////////////
 
-$from=new DateTime('2013-06-07');
+function downloadPlaylistForChannel($cid, DateTime $from)
+{
 $to=new DateTime();
 //$to=new DateTime('2013-06-18');
 
-$base='https://svenska.yle.fi/spellista/yle-x3m/';
-$cachedir='./cache/';
+$base='https://svenska.yle.fi/spellista/'.$cid.'/';
+$cachedir='./cache/'.$cid.'/';
 
 $artists=array();
 $programs=array();
 $playlists=array();
 $songs=array();
 
-printf("Downloading from %s until %s\n", $from->format('Y-m-d'), $to->format('Y-m-d'));
+printf("Downloading data for %s from %s until %s\n", $cid, $from->format('Y-m-d'), $to->format('Y-m-d'));
 
 $p=new PYle\YleScrobblerParser();
+
 while ($from <= $to) {
  $ymd=$from->format('Y-m-d');
-
- echo "$ymd";
-
- $url=sprintf('%s%s', $base, $ymd);
-
- $cf=$cachedir.$ymd;
- $cached=false;
-
- if (!file_exists($cf)) {
-  echo "-d";
-  $data=file_get_contents($url);
- } else {
-  echo "-c";
-  $cached=true;
-  $data=file_get_contents($cf);
- }
-
- if (!$p->parseHTML($data)) {
-  echo "?";
- } else if (!$cached && ($from<$to)) {
-  // Cache the downloaded data only if parsing was ok
-  echo "!";
-  file_put_contents($cf, $data);
- }
-
- echo "\n";
-
+ parse_playlist($base, $cachedir, $p, $from, $to);
  $s=$p->getPlaylist();
  $playlists[$ymd]=$s;
-
  $from->add(new DateInterval('P1D'));
+ gc_collect_cycles();
 }
 
-$artists=$p->getArtists();
 $programs=$p->getPrograms();
 
 sort($programs);
+$s=new PlaylistCSVSaver($cid);
 
-save_playlists($playlists);
-save_artists($artists);
-save_songs($p->getSongs());
-save_programs($programs);
+$s->save_playlists($playlists);
+$s->save_artists($p->getArtists());
+$s->save_songs($p->getSongs());
+$s->save_programs($programs);
+}
 
+gc_enable();
+
+//$from=new DateTime('2013-06-07');
+//downloadPlaylistForChannel('yle-x3m', $from);
+
+$from=new DateTime('2013-06-09');
+downloadPlaylistForChannel('yle-vega', $from);
 ?>
